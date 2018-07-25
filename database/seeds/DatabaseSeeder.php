5cb602489f4e540cd85d986eecbb04042a5d3739
lucastllima <lucasteixeira-lima@hotmail.com>
2018-07-25 20:48:00 -0300

    update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(CidadesTableSeeder::class);
+        $this->call(RacasTableSeeder::class);
     }
 }
